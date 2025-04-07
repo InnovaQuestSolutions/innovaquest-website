0f5a68600b1f9bf8f0f6d2bd20eee27de1a37be6
Ionut Gecz <ionutgecz@Ionuts-MacBook-Pro.local>
2025-04-07 19:18:06 +0100

    Added SEO robots.txt file and astroconfig file updated

--- astro.config.mjs
+++ astro.config.mjs
@@ -1,15 +1,19 @@
 // @ts-check
 import { defineConfig } from 'astro/config';
-
+import sitemap from '@astrojs/sitemap';
 import tailwind from '@astrojs/tailwind';
 
 import vercel from '@astrojs/vercel';
 // https://astro.build/config
 export default defineConfig({
+  site: 'https://innovaquest.ai',
   image: {
     domains: ["https://images.ctfassets.net"],
   },
-  integrations: [tailwind()],
+  integrations: [
+  tailwind(),
+  sitemap()
+],
   output: "server",
   adapter: vercel(),
 });
